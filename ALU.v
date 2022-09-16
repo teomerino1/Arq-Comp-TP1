e506415926f95a9f958536f44349d3ad8cbdd3dd
Mateo Merino <66422394+teomerino1@users.noreply.github.com>
2022-09-16 18:32:19 -0300

    ALU update

--- ALU.v
+++ ALU.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Julieta Prieto y Mateo Merino
 // 
 // Create Date: 06.09.2022 15:53:00
 // Design Name: 
 // Module Name: ALU
-// Project Name: 
+// Project Name: PipeLine
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -26,14 +26,11 @@ module ALU
 
 )
 (
-    input wire  [(SIZE-1):0] a,
-    input wire [(SIZE-1):0] b,
-    input wire [5:0] opCode,
-    input wire btnA,
-    input wire btnB,
-    input wire btnOP,
-    output wire signed [SIZE:0] res_alu,
-    output wire carry
+    input wire signed  [(SIZE-1):0] i_a_alu,
+    input wire signed [(SIZE-1):0] i_b_alu,
+    input wire [5:0] i_opcode_alu,
+    output wire signed [(SIZE-1):0] o_res_alu,
+    output wire o_carry_alu
     );
 
 localparam ADD  =    6'b100000;
@@ -46,52 +43,46 @@ localparam SRL  =    6'b000010;
 localparam NOR  =    6'b100111;
 
 
-
 reg carry;
 reg signed [SIZE:0] res;
-assign res_alu = res;
+assign o_res_alu = res;
+assign  o_carry_alu = carry;
 
 
+/*
+
+ A wire solo se puede asignar con una asignaci�n de bloqueo continuo. Las asignaciones no bloqueantes ( <= ) solo se pueden hacer a reg . Si las asignaciones de reg s que la unidad done cambian de bloqueo ( = ) a no bloqueo ( <= ), deber�a ser el resultado deseado.
+
+Una asignaci�n sin bloqueo hace una evaluaci�n inmediata con una actualizaci�n retrasada. Esto permite evaluar los valores correctos sin afectar la evaluaci�n de una expresi�n dependiente. Nota: la actualizaci�n retrasada est� en el programador de pasos de tiempo verilog; no tendr� un retraso visible en la forma de onda.
+
+Use las asignaciones no bloqueantes en reg que desea tratar como un flop. Cualquier reg usado para la l�gica combinacional o la generaci�n de reloj debe usar el bloqueo. Habr� condiciones de carrera si el bloqueo no se utiliza para la l�gica combinacional o el reloj. 
+*/
 
 
-
-
-a
-
-
-
-always @(*) 
+always @(*) //i_opcode_alu, i_a_alu, i_b_alu (?)
 
 begin
-    case(opCode)
+    case(i_opcode_alu)
 
                          
         ADD: begin
 
-             res <= {1'b0 , a} + {1'b0 ,b} ; 
-             carry <= (  res[SIZE] &  {1,{ (SIZE-1){0} } } == {1,{ (SIZE-1){0} } }  )  ? 1'b1 : 1'b0; // si el msb es 1 hay carry y prendemos el led carry
-             
+             res <= i_a_alu + i_b_alu ; 
+             carry <= (  res[SIZE-1]  ^ i_a_alu[SIZE-1] ^ i_b_alu [SIZE-1] ) & res[SIZE-1]; // si el msb es 1 hay carry y prendemos el led carry
+             //si a y b son positivos y res me quedo negativo hubo carry
              end
 
-        SUB:    res <= {1'b0 , a} - {1'b0 ,b};
-        AND:    res <= {1'b0 , a} & {1'b0 ,b} ;
-        OR :    res <= {1'b0 , a} | {1'b0 ,b} ;
-        XOR:    res <= {1'b0 , a} ^ {1'b0 ,b};
-        SRA:    res <= {1'b0 , a} >>> {1'b0 ,b};
-        SRL:    res <= {1'b0 , a} >> {1'b0 ,b} ;
-        NOR:    res <= ~({1'b0 , a} | {1'b0 ,b} );
+        SUB:    res <= i_a_alu - i_b_alu ;
+        AND:    res <= i_a_alu & i_b_alu ;
+        OR :    res <= i_a_alu | i_b_alu ;
+        XOR:    res <= i_a_alu ^ i_b_alu ;
+        SRA:    res <= i_a_alu >>> 1 ;
+        SRL:    res <= i_a_alu >>  1 ;
+        NOR:    res <= ~(i_a_alu | i_b_alu );
         default: res <= {SIZE{0}};
     endcase
 end
 
 
-/*
-
- A wire solo se puede asignar con una asignación de bloqueo continuo. Las asignaciones no bloqueantes ( <= ) solo se pueden hacer a reg . Si las asignaciones de reg s que la unidad done cambian de bloqueo ( = ) a no bloqueo ( <= ), debería ser el resultado deseado.
-
-Una asignación sin bloqueo hace una evaluación inmediata con una actualización retrasada. Esto permite evaluar los valores correctos sin afectar la evaluación de una expresión dependiente. Nota: la actualización retrasada está en el programador de pasos de tiempo verilog; no tendrá un retraso visible en la forma de onda.
-
-Use las asignaciones no bloqueantes en reg que desea tratar como un flop. Cualquier reg usado para la lógica combinacional o la generación de reloj debe usar el bloqueo. Habrá condiciones de carrera si el bloqueo no se utiliza para la lógica combinacional o el reloj. 
-*/
       
 endmodule
