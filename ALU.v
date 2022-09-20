5f7620c162e9bc2480c74d3e899de59eb192baad
Mateo Merino <66422394+teomerino1@users.noreply.github.com>
2022-09-20 18:25:30 -0300

    Update ALU.v
    Cambios finales

--- ALU.v
+++ ALU.v
@@ -22,7 +22,7 @@
 
 module ALU
 #(
-    parameter SIZE= 8
+    parameter SIZE= 9
 
 )
 (
@@ -44,18 +44,18 @@ localparam NOR  =    6'b100111;
 
 
 reg carry;
-reg signed [SIZE:0] res;
+reg signed [(SIZE-1):0] res;
 assign o_res_alu = res;
 assign  o_carry_alu = carry;
 
 
 /*
 
- A wire solo se puede asignar con una asignaci�n de bloqueo continuo. Las asignaciones no bloqueantes ( <= ) solo se pueden hacer a reg . Si las asignaciones de reg s que la unidad done cambian de bloqueo ( = ) a no bloqueo ( <= ), deber�a ser el resultado deseado.
+ A wire solo se puede asignar con una asignaciï¿½n de bloqueo continuo. Las asignaciones no bloqueantes ( <= ) solo se pueden hacer a reg . Si las asignaciones de reg s que la unidad done cambian de bloqueo ( = ) a no bloqueo ( <= ), deberï¿½a ser el resultado deseado.
 
-Una asignaci�n sin bloqueo hace una evaluaci�n inmediata con una actualizaci�n retrasada. Esto permite evaluar los valores correctos sin afectar la evaluaci�n de una expresi�n dependiente. Nota: la actualizaci�n retrasada est� en el programador de pasos de tiempo verilog; no tendr� un retraso visible en la forma de onda.
+Una asignaciï¿½n sin bloqueo hace una evaluaciï¿½n inmediata con una actualizaciï¿½n retrasada. Esto permite evaluar los valores correctos sin afectar la evaluaciï¿½n de una expresiï¿½n dependiente. Nota: la actualizaciï¿½n retrasada estï¿½ en el programador de pasos de tiempo verilog; no tendrï¿½ un retraso visible en la forma de onda.
 
-Use las asignaciones no bloqueantes en reg que desea tratar como un flop. Cualquier reg usado para la l�gica combinacional o la generaci�n de reloj debe usar el bloqueo. Habr� condiciones de carrera si el bloqueo no se utiliza para la l�gica combinacional o el reloj. 
+Use las asignaciones no bloqueantes en reg que desea tratar como un flop. Cualquier reg usado para la lï¿½gica combinacional o la generaciï¿½n de reloj debe usar el bloqueo. Habrï¿½ condiciones de carrera si el bloqueo no se utiliza para la lï¿½gica combinacional o el reloj. 
 */
 
 
@@ -69,17 +69,41 @@ begin
 
              res <= i_a_alu + i_b_alu ; 
              carry <= (  res[SIZE-1]  ^ i_a_alu[SIZE-1] ^ i_b_alu [SIZE-1] ) & res[SIZE-1]; // si el msb es 1 hay carry y prendemos el led carry
+           
+            if ((i_a_alu[SIZE-1] & i_b_alu [SIZE-1]) == 1 ) //Cuando los dos operandos son negativos; el bit de signo permanece negativo
+                 res[SIZE-1] <=1'b1;
+                 carry <=1'b0;
              //si a y b son positivos y res me quedo negativo hubo carry
              end
-
-        SUB:    res <= i_a_alu - i_b_alu ;
-        AND:    res <= i_a_alu & i_b_alu ;
-        OR :    res <= i_a_alu | i_b_alu ;
-        XOR:    res <= i_a_alu ^ i_b_alu ;
-        SRA:    res <= i_a_alu >>> 1 ;
-        SRL:    res <= i_a_alu >>  1 ;
-        NOR:    res <= ~(i_a_alu | i_b_alu );
-        default: res <= {SIZE{0}};
+        SUB: begin    
+            carry <=1'b0;
+            res <= i_a_alu - i_b_alu ;
+            end
+        AND: begin
+            carry <=1'b0;
+            res <= i_a_alu & i_b_alu ;
+            end
+        OR : begin
+            carry <=1'b0;
+            res <= i_a_alu | i_b_alu ;
+            end
+        XOR: begin
+            carry <=1'b0;
+            res <= i_a_alu ^ i_b_alu ;
+            end
+        SRA: begin
+            carry <=1'b0;
+            res <= i_a_alu >>> 1 ;
+            end
+        SRL: begin    
+            carry <=1'b0;
+            res <= i_a_alu >>  1 ;
+            end
+        NOR: begin
+            carry <=1'b0;   
+            res <= ~(i_a_alu | i_b_alu );
+            end 
+        //default: res <= {SIZE{0}};
     endcase
 end
 
